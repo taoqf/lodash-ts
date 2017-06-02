a4d7c6c1b4a05cfa25f6dd616ba05ab1ba7d7a93
taoqiufeng <taoqiufeng@ifeidao.com>
2017-06-02 11:49:26 +0800

    extend mixin

--- src/mixin.ts
+++ src/mixin.ts
@@ -33,11 +33,12 @@ export default function mixin<T0 extends {}, T1 extends {}, T2 extends {}, T3 ex
 export default function mixin<T0 extends {}, T1 extends {}, T2 extends {}, T3 extends {}, T4 extends {}, T5 extends {}, T6 extends {}, T7 extends {}, T8 extends {}, T9 extends {}, T10 extends {}, T11 extends {}, T12 extends {}, T13 extends {}, T14 extends {}, T15 extends {}, T16 extends {}, T17 extends {}, T18 extends {}>(s0: T0, s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7, s8: T8, s9: T9, s10: T10, s11: T11, s12: T12, s13: T13, s14: T14, s15: T15, s16: T16, s17: T17, s18: T18): T0 & T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 & T11 & T12 & T13 & T14 & T15 & T16 & T17 & T18;
 export default function mixin<T0 extends {}, T1 extends {}, T2 extends {}, T3 extends {}, T4 extends {}, T5 extends {}, T6 extends {}, T7 extends {}, T8 extends {}, T9 extends {}, T10 extends {}, T11 extends {}, T12 extends {}, T13 extends {}, T14 extends {}, T15 extends {}, T16 extends {}, T17 extends {}, T18 extends {}, T19 extends {}>(s0: T0, s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7, s8: T8, s9: T9, s10: T10, s11: T11, s12: T12, s13: T13, s14: T14, s15: T15, s16: T16, s17: T17, s18: T18, s19: T19): T0 & T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 & T11 & T12 & T13 & T14 & T15 & T16 & T17 & T18 & T19;
 export default function mixin<T0 extends {}, T1 extends {}, T2 extends {}, T3 extends {}, T4 extends {}, T5 extends {}, T6 extends {}, T7 extends {}, T8 extends {}, T9 extends {}, T10 extends {}, T11 extends {}, T12 extends {}, T13 extends {}, T14 extends {}, T15 extends {}, T16 extends {}, T17 extends {}, T18 extends {}, T19 extends {}, T20 extends {}>(s0: T0, s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7, s8: T8, s9: T9, s10: T10, s11: T11, s12: T12, s13: T13, s14: T14, s15: T15, s16: T16, s17: T17, s18: T18, s19: T19, s20: T20): T0 & T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 & T11 & T12 & T13 & T14 & T15 & T16 & T17 & T18 & T19 & T20;
-export default function mixin(target: any, ...sources: any[]): any {
+export default function mixin(...sources: any[]): any;
+export default function mixin(...sources: any[]): any {
 	return _mixin({
 		deep: false,
 		inherited: true,
 		sources: sources,
-		target: target
+		target: {}
 	});
 }
